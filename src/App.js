6f16d1c43d42811b208bbd03bd4ba8ae5a60ea98
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-21 10:47:20 +0300

    Finished skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Header from "./components/Header";
+import About from "./components/About";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Feedback from "./components/Feedback";
+import Pricing from "./components/Pricing";
+import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Portfolio />
+      <Feedback />
+      <Pricing />
+      <Contact />
     </div>
   );
 }
